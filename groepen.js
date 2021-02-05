1143e1de2a1ccebb41bdd2bedf6931ba3d5b8cc0
DiederikEyckmans <diederik.eyckmans@student.vives.be>
2021-02-05 13:01:41 +0100

    console read toegevoegd

--- groepen.js
+++ groepen.js
@@ -1,6 +1,16 @@
-function giveGroup(gbdate)
-{
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+
+  rl.question('Wat is je geboortedatum? ', (answer) => {
+    giveGroup(answer);
+    rl.close();
+  });
+
+function giveGroup(gbdate) {
     var group = 1 + gbdate % 7;
-    console.log("Je bent toegewezen aan groep " + group);
+    console.log("Je bent toegewezen aan groep " + group)
 }
-giveGroup(2015);
